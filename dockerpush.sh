94e7184b7c6d291b33da8ccce2af836fb1cb83ee
MizzoMazzo <49159800+MizzoMazzo@users.noreply.github.com>
2019-11-19 12:21:40 +0100

    Create dockerpush.sh

---
+++ dockerpush.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+	
+
+ 
+	
+
+set -ex
+	
+
+set -o pipefail
+	
+
+ 
+	
+
+# DockerHub
+	
+
+USERNAME=...
+	
+
+PASSWORD=...
+	
+
+docker login -u $USERNAME -p $PASSWORD
+	
+
+ 
+	
+
+# echo "Building image front-proxy:v21 ..."
+	
+
+# cd ../envoy/production
+	
+
+# docker build -t envoy -f Dockerfile .
+	
+
+# docker tag envoy kameri/envoy:v21
+	
+
+# docker push kameri/envoy:v21
